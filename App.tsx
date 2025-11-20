23a15c107208260a7d4fbfe1bccb9df761defc4b
VkVom <157098477+VkVom@users.noreply.github.com>
2025-11-20 11:43:24 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/auth/LoginScreen';
+
+// Define the navigation types (Industry Standard)
+export type RootStackParamList = {
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
